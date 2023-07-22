eb04384d8e9a09ed4c8fe3b28d1f3bde91aaa9db
sevity <sevity@sevity.com>
2023-07-23 01:01:13 +0900

    update

---
+++ backup/크냐.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    while(true){
+        int a,b;cin >> a >> b;
+        if(a==0&&b==0) break;
+        cout << (a>b?"Yes":"No") << endl;
+    }
+    
+    return 0;
+}
